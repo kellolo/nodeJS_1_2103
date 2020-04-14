ef316055a4cdb9713eb80d41f298c7dd5d32331d
Виталий Шеин <johnnyvoodoo@live.ru>
2020-04-14 23:37:59 +0700

    lesson_5

---
+++ Students/Vitaly Shein/lessons/lesson_5/src/views/shop.vue
@@ -0,0 +1,72 @@
+<template>
+    <div>
+        <header>
+            <div class="logo">E-shop</div>
+            <div class="cart">
+                <form action="#" class="search-form">
+                    <input type="text" class="search-field">
+                    <button class="btn-search" type="submit">
+                        <i class="fas fa-search"></i>
+                    </button>
+                </form>
+                <button class="btn-cart" type="button" @click="showCart = !showCart">Корзина</button>
+                <cart v-show="showCart" ref="cartReference"/>
+            </div>
+        </header>
+        <main>
+            <catalog />
+        </main>
+    </div>
+</template>
+
+<script>
+import catalog from '../components/catalog.vue'
+import cart from '../components/cart.vue'
+
+export default {
+    data() {
+        return {
+            //API: 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses',
+            showCart: false
+        }
+    },
+    components: {
+        catalog, cart
+    },
+    methods: {
+        getData(url) {
+            return fetch(url).then(d => d.json())
+        },
+        postData(url, data) {
+            return fetch(url, {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data) //новый объект с товаром, который нужно добавить
+            }).then(d => d.json())
+        },
+        putData(url, data) {
+            return fetch(url, {
+                method: 'PUT',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data) //дельта-изменение товара +1/-1
+            }).then(d => d.json())
+        },
+        deleteData(url) {
+            return fetch(url, {
+                method: 'DELETE',
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }).then(d => d.json())
+        }
+    }
+}
+</script>
+
+<style lang="sass">
+
+</style>
